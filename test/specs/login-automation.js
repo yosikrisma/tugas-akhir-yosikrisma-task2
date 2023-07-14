66d8542bb8b3db90f9935a5d78d0a8c0c0237cfe
yosi <yosi.krismawardani@aqi.co.id>
2023-07-14 22:52:32 +0700

    add tc1

---
+++ test/specs/login-automation.js
@@ -0,0 +1,5 @@
+describe('Login Aplication',() => {
+    it('should open apps', async() =>{
+await $(/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageView).click();
+    })
+})
--- wdio.conf.js
+++ wdio.conf.js
@@ -99,7 +99,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'localhost',
+    baseUrl: 'http://127.0.0.1',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -115,7 +115,20 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['appium'],
+    services: [
+        [
+            'appium',
+            {
+                args : {
+                    address : '127.0.0.1',
+                    port    : 4723,
+                },
+                logPath : './log',
+                command : 'appium'
+
+            }
+        ]
+    ],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
